dfdb51ae2d69bd3f2bc09f3fab41c556dd65d3a5
Ilua SERGEEV <iluasergeev@Iluas-MacBook-Pro.local>
2023-10-17 21:50:27 +0300

    Fixed forms exports

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export * from './src/objs/index.js';
 export * from './src/hooks/index.js';
 export * from './src/handlers/index.js';
 export * from './src/enums/index.js';
+export * from './src/forms/index.js';
